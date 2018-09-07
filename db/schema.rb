2581281c15f67f152642085126793b523e8c68e5
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2018-09-07 12:03:02 -0500

    Created a Book model and migration with the specified fields. Made the fields not null in DB but also required in the model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906164044) do
+ActiveRecord::Schema.define(version: 20180906225330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "genre", default: "", null: false
+    t.string "author", default: "", null: false
+    t.string "image", default: "", null: false
+    t.string "title", default: "", null: false
+    t.string "publisher", default: "", null: false
+    t.string "year", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
