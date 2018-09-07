f24f23c4d6ca46bc04f212e47073cfde9bbad9b4
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2018-09-07 12:08:33 -0500

    Forgot to run Rubocop before the commit. Some indentation errors thrown by Rubocop were fixed in this commit.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,8 @@
 class Book < ApplicationRecord
-	validates :genre, presence: true # Make field required
-	validates :author, presence: true # Make field required
-	validates :image, presence: true # Make field required
-	validates :title, presence: true # Make field required
-	validates :publisher, presence: true # Make field required
-	validates :year, presence: true # Make field required
+  validates :genre, presence: true # Make field required
+  validates :author, presence: true # Make field required
+  validates :image, presence: true # Make field required
+  validates :title, presence: true # Make field required
+  validates :publisher, presence: true # Make field required
+  validates :year, presence: true # Make field required
 end
