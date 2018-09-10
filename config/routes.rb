1b0b43d728fd88c4ed7484cfcaa69d4c683e8775
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2018-09-10 10:59:31 -0500

    Recreate DB to only have Users table. Fixed a comment-line

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users #It adds all CRUD routes for a User
+  devise_for :users #I t adds all CRUD routes for a User
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
