b094df52071c02f21ea3fa2f2432b18f3a8f74b0
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2018-09-12 14:42:24 -0500

    RSpec command was missing in travis. Tests wouldn't had run without selecting test framework and library. Book attributes were missing in the factory

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :book do
-    
+    genre { Faker::Book.genre }
+    author { Faker::Book.author }
+    image { Faker::Internet.url }
+    title { Faker::Book.title }
+    publisher { Faker::Book.publisher }
+    year { Faker::Number.between(1900, 2018) }
   end
 end
