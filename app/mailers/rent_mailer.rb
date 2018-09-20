d620194ab837a525cec5485a7522b569d1ca739a
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2018-09-20 17:30:08 -0500

    Email with the rent details. Asyncronous job and hidden credentials

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -4,8 +4,16 @@ class RentMailer < ApplicationMailer
   #
   #   en.rent_mailer.rent_created.subject
   #
+<<<<<<< HEAD
   def rent_created(rent_id)
     @rent = Rent.find(rent_id)
     mail to: @rent.user.email
+=======
+  def rent_created
+    @rent = Rent.last
+    @book_rented = Book.find(@rent.book_id)
+    @user_renting = User.find(@rent.user_id)
+    mail to: @user_renting.email
+>>>>>>> Email with the rent details. Asyncronous job and hidden credentials
   end
 end
