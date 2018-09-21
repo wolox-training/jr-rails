12864257b00aa73ea3ca67444a0c1f0e3a4f3294
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2018-09-21 10:46:25 -0500

    Unneeded worker. Better way to handle parameters. Production env variable were missing

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -4,16 +4,8 @@ class RentMailer < ApplicationMailer
   #
   #   en.rent_mailer.rent_created.subject
   #
-<<<<<<< HEAD
   def rent_created(rent_id)
     @rent = Rent.find(rent_id)
     mail to: @rent.user.email
-=======
-  def rent_created
-    @rent = Rent.last
-    @book_rented = Book.find(@rent.book_id)
-    @user_renting = User.find(@rent.user_id)
-    mail to: @user_renting.email
->>>>>>> Email with the rent details. Asyncronous job and hidden credentials
   end
 end
