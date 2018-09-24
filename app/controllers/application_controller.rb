a4429e9a14e3bc8a3cebf0e8fc111c4edcee3b8e
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2018-09-24 11:20:03 -0500

    Default locale needed. Better syntax for the try of current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = current_user.try(:locale) || I18n.default_locale
+    I18n.locale = current_user.&locale || I18n.default_locale
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20180921214123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "verification_code", null: false
-    t.string "locale"
+    t.string "locale", default: "es", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
