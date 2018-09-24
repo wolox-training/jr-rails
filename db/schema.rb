2a6f96facc172f2c6099288a3d5e401bc83bcc2a
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2018-09-24 08:27:04 -0500

    Language location settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180911224654) do
+ActiveRecord::Schema.define(version: 20180921214123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180911224654) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "verification_code", null: false
+    t.string "locale"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
