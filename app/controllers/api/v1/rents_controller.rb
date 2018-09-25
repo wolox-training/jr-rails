f58ca2a61b07f24ab3dada9dba6e33da8c60ccf2
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2018-09-25 14:41:32 -0500

    Basic structure of the Rents policy

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -4,6 +4,9 @@ module Api
   module V1
     class RentsController < ApiController
       include Wor::Paginate
+      before_action :authenticate_user!
+
+      after_action :verify_authorized
       def index
         rents = policy_scope(Rent)
         render_paginated rents
